71aaa9d10ec3740d8cdca72d8d3f1a1e21cc7848
Tyler Matteo <tylermatteo434@gmail.com>
2017-09-28 23:20:35 -0400

    Builds landing page map.

--- src/pages/start-game/start-game.ts
+++ src/pages/start-game/start-game.ts
@@ -25,20 +25,9 @@ import SVG from 'svg.js';
   ]
 })
 export class StartGamePage {
-  width:number;
-  height:number;
-  recs:any[];
-  roadY:number[];
-  roadX:number[];
-  visibleState:string;
   taskCount:number;
 
   constructor(public navCtrl: NavController, private alertCtrl: AlertController, public task: Task) {
-    this.width = 3300;
-    this.height = 3000;
-    this.roadY = [505];
-    this.roadX = [707];
-    this.visibleState = 'normal';
     this.taskCount = task.incomplete().length;
   }
 
@@ -46,44 +35,7 @@ export class StartGamePage {
     this.navCtrl.push('tasks');
   }
 
-  showSchool() {
-    this.visibleState = 'right';
-    setTimeout(() => {
-      this.visibleState = 'normal';
-    }, 200);
-
-    setTimeout(() => {
-      this.visibleState = 'left';
-    }, 400);
-
-    setTimeout(() => {
-      this.visibleState = 'normal';
-    }, 500);
-    //this.navCtrl.push('SchoolPage');
-    //this.navCtrl.push('StockExchangePage');
-    //this.navCtrl.push('BankPage');
-   // this.navCtrl.push('TownHallPage');
-
-  }
   ionViewDidLoad() {
-    const draw = SVG('drawing').size(this.width, this.height).viewbox(0,0,this.width,this.height).rotate(-45).scale(1, .7);
-    for( let x = 0; x < this.width; x+=101 ) {
-      for( let y = 0; y < this.height; y+=101 ) {
-        draw.rect(100, 100).move(x, y).attr({ fill: '#99CC33' });
-      }
-    }
-
-    for( const key in this.roadY ) {
-      for( let y = 0; y < this.height; y+=101 ) {
-        draw.image('../assets/img/roadY.png').size(100, 100).move(this.roadY[key], y);
-      }
-    }
-
-    for( const key in this.roadX ) {
-      for( let x = 0; x < this.width; x+=101 ) {
-        draw.image('../assets/img/roadX.png').size(100, 100).move(x,this.roadX[key]);
-      }
-    }
 
     let alert = this.alertCtrl.create({
       title: 'Welcome!',
